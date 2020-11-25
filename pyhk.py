fcf9d7b8197faaaecd6e021b81da2c4fc89fbd7b
Max Suica <max.suica@gmail.com>
2020-11-25 10:29:18 -0800

    Push License into standalone file

--- pyhk.py
+++ pyhk.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python
-# pyhk.py
-#
-# Copyright (C) 2011 Michael Schurpf
-# E-mail: michaelschuerpf AT gmail DOT com
-#
+import ctypes, pyWinhook, pythoncom
+
 # Extends pyWinhook to have normal hotkey functionality like autohotkey (AHK) scripts.
 # Hotkeys have to be entered in lists! Even if single items.
 # Hotkeys can be entered in ID list or human readble.
@@ -16,35 +12,6 @@
 # mouse move, mouse wheel up, mouse wheel down can only be used on its own
 # Example: ['mouse wheel down']
 # Not working ['mouse wheel down','4']
-#
-# Scripts maintained at http://www.swisstard.com
-# Comments, suggestions and bug reports welcome.
-#
-# Released subject to the GNU Public License
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# In addition to the permissions in the GNU General Public License, the
-# authors give you unlimited permission to link or embed the compiled
-# version of this file into combinations with other programs, and to
-# distribute those combinations without any restriction coming from the
-# use of this file. (The General Public License restrictions do apply in
-# other respects; for example, they cover modification of the file, and
-# distribution when not linked into a combine executable.)
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-import ctypes, pyWinhook, pythoncom  #thread
-
 
 class pyhk:
     """Hotkey class extending pyWinhook"""
